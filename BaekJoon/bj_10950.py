6fdef71711e2b412560093c27fcaa0f3ce35d8bf
이지호 <cctv1998@naver.com>
2021-06-10 14:06:50 +0900

    beakjoon : 10950번 A+B-3

---
+++ BaekJoon/bj_10950.py
@@ -0,0 +1,12 @@
+T = int(input())
+# K = []
+# for i in range(T):
+#     A,B=map(int,input().split())
+#     K.append(A+B)
+
+# for a in range(T):
+#     print(K[a])
+
+for i in range(T):
+    A,B=map(int,input().split())  
+    print(A+B)
