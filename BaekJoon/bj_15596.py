7958eb5ecc530ab66830111ef9ebf20b38a66ab3
이지호 <cctv1998@naver.com>
2021-07-28 17:31:57 +0900

    Beakjoon : 4673번 셀프 넘버

--- BaekJoon/bj_15596.py
+++ BaekJoon/bj_15596.py
@@ -1,6 +1,14 @@
-def solve(a):
-    ans = 0
-    for i in a:
-        ans = ans + i
-
+def notSelfNum(A):
+    ans = A + A//1000 + ((A%1000)//100) + ((A%100)//10) + (A%10)
     return ans
+
+
+    
+T=[]
+K = set()
+for i in range (10000):
+    K.add(notSelfNum(i))
+
+for j in range(10000):
+    if j not in K:
+        print(j)
