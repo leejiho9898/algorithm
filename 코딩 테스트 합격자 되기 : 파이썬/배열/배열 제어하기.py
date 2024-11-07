0a2389af160fcb38c7df57322749834c4395bb41
leejiho9898 <cctv1998@naver.com>
2024-11-07 16:46:44 +0900

    배열 제어하기

---
+++ 코딩 테스트 합격자 되기 : 파이썬/배열/배열 제어하기.py
@@ -0,0 +1,14 @@
+# 배열의 중복값 제거하고 내림차순 정렬하기
+
+# 문제 풀기전 분석 
+#  1. 배열의 중복값을 제거한다.
+#  2. 배열을 내림차순으로 정렬한다.
+
+def solution(arr:list):
+    unique_arr = list(set(arr))
+    unique_arr.sort(reverse=True)
+    return unique_arr
+
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+print(solution([4, 2, 2, 1, 3, 4])) # 반환값 : [4, 3, 2, 1]
+print(solution([2, 1, 1, 3, 2, 5, 4])) # 반환값 : [5, 4, 3, 2, 1]
